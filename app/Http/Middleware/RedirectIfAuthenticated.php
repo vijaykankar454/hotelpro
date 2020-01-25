d0ca4240a14a2ff9eb026bbcde290a71eec09d25
vijay <vijay.kankar@homeonline.com>
2020-01-25 14:19:21 +0530

    update

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,9 +18,13 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        if ($guard == "admin" && Auth::guard($guard)->check()) {
+            return redirect(RouteServiceProvider::ADMIN);
+        }
         if (Auth::guard($guard)->check()) {
             return redirect(RouteServiceProvider::HOME);
         }
+      
 
         return $next($request);
     }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+
+Route::get('/login/admin', 'Auth\LoginController@showAdminLoginForm');
+  
+Route::get('/register/admin', 'Auth\RegisterController@showAdminRegisterForm');
+  
+
+Route::post('/login/admin', 'Auth\LoginController@adminLogin');
+  
+Route::post('/register/admin', 'Auth\RegisterController@createAdmin');
+   
+
+
+Route::view('/admin', 'admin');
